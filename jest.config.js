9755a792f7d187344627eaaf2e59a8858b7fa0a6
Giovani Anhesini Bezerra <giovani_bezerra@apple.com>
2020-09-25 09:06:32 -0300

    fix: typo errors

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,14 @@ const { resolve } = require('path');
 const root = resolve(__dirname);
 
 module.exports = {
-    rootDir: root,
-    displayname: 'root-tests',
-    testMatch: ['<rootDir>/src/**/*.test.ts'],
-    testEnviroment: 'node',
-    clearMocks: true,
-    preset: 'ts-jest',
-    moduleNameMapper: {
-        '@src/(.*)': '<rootDir>/src/$1,',
-        '@test/(.*)': '<rootDir>/test/$1,'
-    }
-}
+  rootDir: root,
+  displayName: 'root-tests',
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  testEnvironment: 'node',
+  clearMocks: true,
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': '<rootDir>/test/$1',
+  },
+};
